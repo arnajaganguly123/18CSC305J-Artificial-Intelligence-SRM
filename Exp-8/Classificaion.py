ac8f2001cf537493a0c58d3f2e92a2958c810c91
Arnaja Ganguly <79324117+arnajaganguly123@users.noreply.github.com>
2023-04-17 11:25:34 +0530

    Update Classificaion.py

--- Exp-8/Classificaion.py
+++ Exp-8/Classificaion.py
@@ -1,36 +1,14 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[15]:
-
-
-get_ipython().run_line_magic('matplotlib', 'inline')
+%matplotlib inline
 import pandas as pd
 import matplotlib.pyplot as plt
 fruits = pd.read_table('fruit_data_with_colors.txt')
 fruits.head()
 
-
-# In[16]:
-
-
 print(fruits.shape)
-
-
-# In[17]:
-
 
 print(fruits['fruit_name'].unique())
 
-
-# In[18]:
-
-
 print(fruits.groupby('fruit_name').size())
-
-
-# In[19]:
-
 
 fruits.drop('fruit_label', axis=1).plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False, figsize=(9,9), 
                                         title='Box Plot for each input variable')
@@ -38,17 +16,11 @@ plt.savefig('fruits_box')
 plt.show()
 
 
-# In[20]:
-
-
 import pylab as pl
 fruits.drop('fruit_label' ,axis=1).hist(bins=30, figsize=(9,9))
 pl.suptitle("Histogram for each numeric input variable")
 plt.savefig('fruits_hist')
 plt.show()
-
-
-# In[25]:
 
 
 from pandas.plotting import scatter_matrix
@@ -60,4 +32,3 @@ cmap = cm.get_cmap('gnuplot')
 scatter =scatter_matrix(X, c = y, marker = 'o', s=40, hist_kwds={'bins':15}, figsize=(9,9), cmap = cmap)
 plt.suptitle('Scatter-matrix for each input variable')
 plt.savefig('fruits_scatter_matrix')
-
